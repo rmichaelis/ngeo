92248a2406a0559c5df49707f81804452f0d0a47
Frederic Junod <frederic.junod@camptocamp.com>
2016-01-25 14:17:55 +0100

    Rename appModule to app.module

--- contribs/gmf/apps/mobile/js/controller.js
+++ contribs/gmf/apps/mobile/js/controller.js
@@ -51,4 +51,4 @@ goog.inherits(app.MobileController, gmf.AbstractMobileController);
 
 
 
-appModule.controller('MobileController', app.MobileController);
+app.module.controller('MobileController', app.MobileController);
